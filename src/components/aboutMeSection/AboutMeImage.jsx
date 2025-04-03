ae7cb9e2b2179aba01bf52dca70a15fe77d7bf29
Jasur <rakhmatovjasur3@gmail.com>
2025-04-03 14:42:30 +0500

    proffesional

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -1,11 +1,11 @@
- import about from "../../public/images/about-me.jpg";
+
 const AboutMeImage = () => {
 
   return (
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-          src={about}
+          src="/public/images/about-me.jpg"
           alt="About Me Image"
           className="h-full w-auto object-cover"
         />
