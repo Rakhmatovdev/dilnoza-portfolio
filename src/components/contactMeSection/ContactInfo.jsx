fd389a189bfdd99dd92bbc120109b7bfb59df015
Jasur <rakhmatovjasur3@gmail.com>
2025-04-03 14:39:18 +0500

    life

--- src/components/contactMeSection/ContactInfo.jsx
+++ src/components/contactMeSection/ContactInfo.jsx
@@ -6,7 +6,7 @@ import SingleInfo from "./SingleInfo";
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-4">
-      <SingleInfo text="dilnoza.turkmenbayeva@gmail.com" Image={HiOutlineMail} />
+      <SingleInfo text="dilnozaolimzhonovna1@icloud.com" Image={HiOutlineMail} />
       <SingleInfo text="+998 99 624 19 44" Image={FiPhone} />
       <SingleInfo text="Uzbekistan, Tashkent" Image={IoLocationOutline} />
     </div>
