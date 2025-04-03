234a389b6b6d66b8d47f1de39a88447565decf8f
Jasur <rakhmatovjasur3@gmail.com>
2025-04-03 15:04:13 +0500

    fix bugs

--- public/index.js
+++ public/index.js
@@ -4,7 +4,6 @@ import email from './images/email-image.png';
 import about from './images/about-me.jpg';
 import subSkills from './images/subSkills.jpg';
 import experience from './images/experience-image.png';
-import contact from './images/contact-image.png';
 import skills from './images/skills.avif';
 import website1 from './images/website-img-1.jpg';
 import website2 from './images/website-img-2.webp';
@@ -12,4 +11,4 @@ import website3 from './images/website-img-3.jpg';
 import website4 from './images/website-img-4.jpg';
 
 
-export const Images = {dilnoza,dilnoza2,email,about,subSkills,experience,contact,skills,website1,website2,website3,website4};
+export const Images = {dilnoza,dilnoza2,email,about,subSkills,experience,skills,website1,website2,website3,website4};
